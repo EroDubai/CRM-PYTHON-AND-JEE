f5fe9ca74c3ecc901f3b6c3ba718dc51101b55f6
EroDubai <daalpiva@gmail.com>
2017-10-08 06:56:15 -0500

    commit4.0

--- class/urls.py
+++ class/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = "index"),
+    #/class/
+    url(r'^$', views.index, name = 'index'),
+    #/music/712/
+    url(r'^(?P<montaje_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- class/views.py
+++ class/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello world</h1>")
+
+def detail(request,montaje_id):
+    return HttpResponse("<h2>details of the montaje id:" + str(montaje_id) + "</h2>");
